294edee5e3b668dc33ab235a76e32ce1873859e6
TuantvSe63218 <48800909+TuantvSe63218@users.noreply.github.com>
2021-03-04 09:09:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import React, { Component } from 'react';
 import Layout from './Layout/Layout';
-// import SignIn from './screens//sign-in/SignIn';
+import Login from './screens/login/Login';
+import {history} from './store/helper/history'
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { PrivateRoute } from './store/component/PrivateRoute'
+
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Layout />
+                <Router history={history}>
+                    <ReactNotification />
+                    {/* <Switch> */}
+                    <div>
+                        <PrivateRoute exact path="/" component={Layout} />
+                        <Route path="/login" component={Login} />
+                    </div>
+                    {/* </Switch> */}
+                </Router>
                 {/* <SignIn/> */}
             </div>
         );
