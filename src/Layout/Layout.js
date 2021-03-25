48ec1ec66416d6390f551cd9747773fa19d9353f
Nghinh-Tri <52157962+Nghinh-Tri@users.noreply.github.com>
2021-03-25 16:10:18 +0700

    firebase test

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,9 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import Navigation from '../component/navigation/Navigation';
 import NavBar from '../component/nav-bar/NavBar';
 import RouteList from '../RouterMap'
-import { checkSession } from '../service/action/AuthenticateAction';
+import firebase from "../service/firebase/firebase";
 
-class Layout extends Component {    
+class Layout extends Component {
+
+    componentDidMount = () => {
+        const messaging = firebase.messaging()
+        messaging.requestPermission().then(() => {
+            return messaging.getToken()
+        }).then(token => {
+            console.log('token', token)
+        })
+    }
 
     showContent = (RouteList) => {
         var result = null;
---
+++ src/service/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAL89OLoHUyjPc0QVMZfqMxVDx6p73HJRs",
+    authDomain: "esms-1e15a.firebaseapp.com",
+    projectId: "esms-1e15a",
+    storageBucket: "esms-1e15a.appspot.com",
+    messagingSenderId: "239302255847",
+    appId: "1:239302255847:web:1a4b9ef59c02236ded6e42"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
