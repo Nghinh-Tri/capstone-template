1a5123dd38b70861b9572e277cd228c51c64db2d
Nghinh-Tri <52157962+Nghinh-Tri@users.noreply.github.com>
2021-04-08 09:27:47 +0700

    Update LoginPage.js

--- src/screen/LoginPage/LoginPage.js
+++ src/screen/LoginPage/LoginPage.js
@@ -37,7 +37,7 @@ class Login extends Component {
                             <div className="row justify-content-center my-auto">
                                 <div className="col-md-8 col-10 my-5">
                                     <div className="row justify-content-center px-3 mb-3"> <img id="logo" src="https://i.imgur.com/PSXxjNY.png" /> </div>
-                                    <h3 className="mb-5 text-center heading">Hello Abcd1234$</h3>
+                                    <h3 className="mb-5 text-center heading">Hello</h3>
                                     <h6 className="msg-info text-center">Please login to your account</h6>
 
                                     <form onSubmit={this.handleSubmit} >
@@ -71,7 +71,7 @@ class Login extends Component {
                         </div>
                         <div className="card card2">
                             <div className="my-auto mx-md-5 px-md-5 right">
-                                <h3 className="text-white">We are more than just a company</h3> <small className="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+                                <h3 className="text-white">We are more than just a company</h3> 
                             </div>
                         </div>
                     </div>
