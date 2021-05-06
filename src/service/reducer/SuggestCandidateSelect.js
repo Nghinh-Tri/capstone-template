4d7ba7beed3a553f6e86fe1870936f674f84c572
Nghinh-Tri <52157962+Nghinh-Tri@users.noreply.github.com>
2021-05-06 09:56:42 +0700

    Update SuggestCandidateSelect.js

--- src/service/reducer/SuggestCandidateSelect.js
+++ src/service/reducer/SuggestCandidateSelect.js
@@ -7,6 +7,18 @@ const SuggestCandidateSelect = (state = initState, action) => {
         case SUGGEST_CANDIDATE.SET_SELECT_POSITION:
             state = action.index
             return state
+
+        case SUGGEST_CANDIDATE.CONFIRM_SUGGEST:
+            state = 0
+            return [...state]
+
+        case SUGGEST_CANDIDATE.CONFIRM_SUGGEST_FAIL:
+            state = 0
+            return [...state]
+
+        case SUGGEST_CANDIDATE.CANCEL_SUGGEST:
+            state = 0
+            return [...state]
         default:
             return state;
     }
