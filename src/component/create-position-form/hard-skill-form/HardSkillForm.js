0be3eeb8a9134f3044bf8b339e83b1a067b390d3
Nghinh-Tri <52157962+Nghinh-Tri@users.noreply.github.com>
2021-05-13 17:22:38 +0700

    add star in require field on assign skill

--- src/component/create-position-form/hard-skill-form/HardSkillForm.js
+++ src/component/create-position-form/hard-skill-form/HardSkillForm.js
@@ -84,7 +84,7 @@ class HardSkillForm extends Component {
         return (
             <div class="card mb-4">
                 <div class="card-header">
-                    <i class="fas fa-table mr-1"></i>Hard Skills
+                    <i class="fas fa-table mr-1"></i>Hard Skills <span style={{ color: 'red', fontWeight: 500 }} >*</span>
                     <span className="material-icons pull-right clear" style={{ cursor: 'pointer' }} onClick={this.setMinimize} >
                         {!this.state.isMinimize ? 'minimize' : 'crop_free'}
                     </span>
